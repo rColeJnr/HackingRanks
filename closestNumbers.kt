1292284e63138bde352466c5ff8066b6cbac8f68
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-22 00:00:42 +0300

    closestNumbers solution

--- closestNumbers.kt
+++ closestNumbers.kt
@@ -7,5 +7,23 @@ fun main(args: Array<String>): Unit {
 fun closestNumbers(arr: Array<Int>): Array<Int> {
     // Write your code here
 
+    if(arr.size <= 1) return arr
     
+    val sArr = arr.sortedArray()
+    val result = mutableListOf<Int>()
+
+    var minDiff = sArr[1] - sArr[0]
+
+    for (num in 0 until arr.size-1){
+        var diff = sArr[num+1] - sArr[num]
+        if ( diff < minDiff ) {
+            minDiff = diff
+        }
+        if ( diff == minDiff ) {
+            result.add(sArr[num])
+            result.add(sArr[num+1])
+        }
+    }
+    
+    return result.toTypedArray()
 }
