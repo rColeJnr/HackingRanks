8168ff7e3d8ebb7f9ac5b0ce92f11d54b0d8ffc4
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-27 16:49:24 +0300

    Maxmin initial commit

---
+++ minMax.kt
@@ -0,0 +1,5 @@
+// You will be given a list of integers, , and a single integer . You must create an array of length  from elements of  such that its unfairness is minimized. Call that array . Unfairness of an array is calculated as
+
+fun maxMin(k: Int, arr: Array<Int>): Int {
+    
+}
