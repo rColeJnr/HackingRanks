c39c8efbd833dd6f8c9ed2f80e47868546ebafc3
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-27 17:03:20 +0300

    Strong password initial commit

---
+++ strongPassword.kt
@@ -0,0 +1,7 @@
+// check if password meets criteria to be considered a storng password
+// return the number of characters to be added to meet minimum requirement
+fun minimumNumber(n: Int, p: String): Int {
+
+    
+
+}
