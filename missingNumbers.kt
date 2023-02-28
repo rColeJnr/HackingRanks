f9affd3d264735682ea1a32004a5c08c5b26b357
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-28 11:15:52 +0300

    Missing numbers brute force solution

--- missingNumbers.kt
+++ missingNumbers.kt
@@ -2,6 +2,13 @@
 
 fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
 
+    // Write your code here
+    val sB = brr.sortedArray()
+    val result = MutableList(brr.size) { a -> sB[a] }
+    for (a in arr) {
+        result.remove(a)
+    }
     
+    return result
 
 }
