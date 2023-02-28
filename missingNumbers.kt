7a25f9bc32348ce31ef42051e21f04b25679e28b
rColeJnr <ricardo.m.jnr@gmail.com>
2023-02-28 12:31:11 +0300

    Missing number efficient solution

--- missingNumbers.kt
+++ missingNumbers.kt
@@ -1,14 +1,30 @@
 // Given two arrays of integers, find which elements in the second array are missing from the first array.
 
 fun missingNumbers(arr: Array<Int>, brr: Array<Int>): Array<Int> {
-
     // Write your code here
-    val sB = brr.sortedArray()
-    val result = MutableList(brr.size) { a -> sB[a] }
-    for (a in arr) {
-        result.remove(a)
+
+    val bMap = mutableMapOf<Int, Int>() 
+    val ll = mutableListOf<Int>()
+
+    for (s in brr) {
+        var freq = bMap.get(s) ?: 0
+        bMap.put(s, ++freq)
     }
-    
-    return result
+
+    for (a in arr) {
+        var freq = bMap.get(a)!!
+        bMap[a] = --freq
+    }
+
+    for (b in brr) {
+        var freq = bMap.get(b)!!
+        if (freq != 0) {
+            bMap[b] = --freq
+            ll.add(b)
+        }
+    }
+
+    // idk why we need to call toSortedSet(), 
+    return ll.toTypedArray().toSortedSet().toTypedArray()
 
 }
