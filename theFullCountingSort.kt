4b1f873c2b13f509f87ee8ec8c0e38e55f9ef500
rColeJnr <ricardo.m.jnr@gmail.com>
2023-03-02 10:22:10 +0300

    Counting sort initial commit

---
+++ theFullCountingSort.kt
@@ -0,0 +1,9 @@
+// Use the counting sort to order a list of strings associated with integers. 
+// If two strings are associated with the same integer, they must be printed in their 
+// original order, i.e. your sorting algorithm should be stable. There is one other 
+// twist: strings in the first half of the array are to be replaced with the character - 
+// (dash, ascii 45 decimal).
+
+fun countSort(arr: array<String>) {
+    // Write your code here
+}
