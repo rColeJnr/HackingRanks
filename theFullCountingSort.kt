4e64577f2c0f9061f5288ef8b394dcbd221f3b44
rColeJnr <ricardo.m.jnr@gmail.com>
2023-03-03 03:20:55 +0300

    Counting sort mapping working, code solution, but really really rally bad code

--- theFullCountingSort.kt
+++ theFullCountingSort.kt
@@ -4,13 +4,13 @@
 // twist: strings in the first half of the array are to be replaced with the character - 
 // (dash, ascii 45 decimal).
 
-fun countSort(arr: array<String>) {
+fun countSort(arr: Array<Array<String>>): Unit {
     // Write your code here
     val size = arr.size
     val myMap = mutableMapOf<Int, String>()
     
     for (a in 0 until size/2){
-        arr[a][1] = " -"
+        arr[a][1] = "-"
     }
     
     for (a in 0 until size){
@@ -19,10 +19,15 @@ fun countSort(arr: array<String>) {
         if (value == null) {
             myMap.put( key,  arr[a][1] )
         } else {
-            val work = value + arr[a][1]
+            val work = value + " " + arr[a][1]
             myMap.put(key, work)
         }
     }
     
-    myMap.values.forEach{print("$it ")}
+    val result = Array(size) { "" }
+    
+    for (key in myMap.keys) {
+        result[key] = myMap.get(key)!!
+    }
+    result.forEach { print("$it ") }
 }
